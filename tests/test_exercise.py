6b97bf0f3b91b21a574d0e6b86d3c502497e508e
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 09:16:13 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,34 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["500000","3","1200","325000","2","625000","6"]
+    input_values_stored = ["500000","3","1200","325000","2","625000","6"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert [output[0],output[1],output[2]] == ["Inheritance:","Years since death:","Tax: 56000"]
+    assert [output[3],output[4],output[5]] == ["Inheritance:","Years since death:","Tax: 0"]
+    assert [output[6],output[7],output[8]] == ["Inheritance:","Years since death:","Tax: 24000"]
